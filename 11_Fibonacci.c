a19bd990e99182be4629494dc3918e00af5546ae
Aahib369 <76401832+Aahib369@users.noreply.github.com>
2021-03-18 10:02:44 -0700

    Create 11_Fibonacci.c
    11

---
+++ 11_Fibonacci.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+ 
+int main(){
+   int n, first = 0, second = 1, next, c; //Referance to formulas of the series.
+ 
+   printf("Enter the number of terms\n");
+   scanf("%d",&n); //Scan (read) the number written by user.
+ 
+   printf("First %d terms of Fibonacci series are :-\n",n);
+ 
+   for ( c = 0 ; c < n ; c++ ) //Referance to formulas of the series.
+   {
+      if ( c <= 1 )
+         next = c;
+      else
+      {
+         //Formula to print the series.
+         next = first + second;
+         first = second;
+         second = next;
+      }
+      printf("%d\n",next); //provides the output.
+   }
+   return 0;
+}
