ff3bcf031c2599a1295827ce9d30285870c93bfa
Aahib369 <76401832+Aahib369@users.noreply.github.com>
2021-03-18 09:09:06 -0700

    Create 3_HelloIteration.c
    03

---
+++ 3_HelloIteration.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a = 1;
+    while ( a <= 10 ) //Allow us to print hello world 10 times.
+    {
+        printf ( "Hello World\n" );//Text to be printed.
+        a ++;
+    }
+    return 0;
