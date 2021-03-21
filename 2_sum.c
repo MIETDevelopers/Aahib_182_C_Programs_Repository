ca7c963de043ab0f087643e8eb0511a574055d8a
Aahib369 <76401832+Aahib369@users.noreply.github.com>
2021-03-21 02:15:31 -0700

    Update 2_sum.c

--- 2_sum.c
+++ 2_sum.c
@@ -1,8 +1,8 @@
-#include<stdio.h>            //A basic C program to calculate two numbers.
+#include<stdio.h>
 int main(){
 	int number1, number2, sum;
-	scanf("%d %d", &number1, &number2);
-	sum=number1+number2;
-	printf("Sum of 2 numbers= %d", sum);
+	scanf("%d %d", &number1, &number2);// Scanning values written by user.
+	sum=number1+number2;//Processing the sum operation.
+	printf("Sum of 2 numbers= %d", sum);// Printing the output.
 	return 0;
 }
